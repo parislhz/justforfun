99affef38478f691ed2e7de5d2f4763648020029
刘红藻 <parislhz@gmail.com>
2017-04-14 09:04:17 +0800

    dacong

--- sl_world.py
+++ sl_world.py
@@ -9,7 +9,7 @@ PLANET_X = 512
 PLANET_Y = 1000
 # PLANET_Y = 768
 PLANET_R = 512
-BACKGROUND_COLOR = (255, 255, 255)
+BACKGROUND_COLOR = (150, 150, 100)
 
 
 class PlanetObject(pygame.sprite.Sprite):
@@ -20,20 +20,34 @@ class PlanetObject(pygame.sprite.Sprite):
         self.size = size
         self.planet_angle = 0
         self.img_path = img_path
+        self.orig_surf = pygame.image.load(self.img_path).convert_alpha()
+        self.orig_surf = pygame.transform.scale(self.orig_surf, self.size)
+        self.surf = self.orig_surf
+        self.rect = (0, 0, 0, 0)
         self.calc_abs_pos()
 
     def calc_abs_pos(self):
         theta = self.angle - self.planet_angle
-        surf = pygame.image.load(self.img_path)
-        surf = pygame.transform.scale(surf, self.size)
-        self.surf = pygame.transform.rotate(surf, -theta * 180 / math.pi)
-        x = PLANET_X + PLANET_R * math.sin(theta)
-        y = PLANET_Y - PLANET_R * math.cos(theta)
+        self.surf = pygame.transform.rotate(self.orig_surf, -theta * 180 / math.pi)
+        # self.surf.set_colorkey((0, 0, 0))
+        x = PLANET_X + self.r * math.sin(theta)
+        y = PLANET_Y - self.r * math.cos(theta)
         self.rect = self.surf.get_rect(center=(x, y))
 
     def update(self, planet_angle):
         self.planet_angle = planet_angle
         self.calc_abs_pos()
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, size, img_path):
+        super(Player, self).__init__()
+        self.size = size
+        self.planet_angle = 0
+        self.img_path = img_path
+        self.surf = pygame.image.load(self.img_path).convert_alpha()
+        self.surf = pygame.transform.scale(self.surf, self.size)
+        self.rect = self.surf.get_rect(center=(PLANET_X, PLANET_Y - PLANET_R))
 
 
 class SunsetLightess:
@@ -44,11 +58,17 @@ class SunsetLightess:
     def __init__(self):
         self.planet_angle = 0
         self.planet_objects = pygame.sprite.Group()
+        player = Player((48, 96), 'res/Bracelet_2.PNG')
+        self.planet_objects.add(player)
+        earth = PlanetObject(0, 0, (1000, 1000), 'res/Earth-PNG-Image.png')
+        self.planet_objects.add(earth)
         obj = PlanetObject(0, 512, (64, 64), 'res/Belt_2.png')
         self.planet_objects.add(obj)
         obj = PlanetObject(10 * math.pi / 180, 650, (64, 32), 'res/Hood_2.png')
         self.planet_objects.add(obj)
-        obj = PlanetObject(-20 * math.pi / 180, 450, (32, 64), 'res/Sword2.png')
+        obj = PlanetObject(-20 * math.pi / 180, 550, (32, 64), 'res/Sword2.png')
+        self.planet_objects.add(obj)
+        obj = PlanetObject(-30 * math.pi / 180, 800, (128, 128), 'res/Hammer1_3.png')
         self.planet_objects.add(obj)
 
     def run_game(self):
